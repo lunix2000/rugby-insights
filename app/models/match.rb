0cb4e6fe3945a1ae6055516c6928f98b07bcc61b
Galaxiar <shondregrobler@gmail.com>
2026-04-26 15:23:30 +0200

    Update validation
    allow validation to prevent blank inputs

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,2 +1,13 @@
 class Match < ApplicationRecord
+  validates :home_team, :away_team, presence: true
+
+  validates :home_score, :away_score,
+            :possession,
+            :tackles_made,
+            :tackles_missed,
+            :penalties,
+            presence: true,
+            numericality: { greater_than_or_equal_to: 0 }
+
+  validates :possession, numericality: { less_than_or_equal_to: 100 }
 end
